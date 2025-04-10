e9799df1892741c53af949fda8d1377bb5e52047
LindaSosa <admonti@results-in-performance.com>
2025-04-10 17:30:50 -0600

    Update calculatorController.php

--- app/Http/Controllers/Calculator/calculatorController.php
+++ app/Http/Controllers/Calculator/calculatorController.php
@@ -15,27 +15,25 @@ class calculatorController extends Controller
      */
     public function index(LanguageController $languageController)
     {
-        // $audioFiles = [
-        //     'audioIntroPath' => 'calculator_intro_01.mp3',
-        //     'audioConfigPath' => 'calculator_config_04.mp3',
-        //     'audioPartsPath' => 'calculator_parts_02.mp3',
-        //     'audioFunctionsPath' => 'calculator_functions_03.mp3',
-        //     'audioUsePath' => 'calculator_use_05.mp3',
-        //     'audioUnitPath' => 'calculator_unit_06.mp3',
-        //     'audioFractionPath' => 'calculator_fraction_07.mp3',
-        //     'audioHierarchyPath' => 'calculator_hierarchy_08.mp3',
-        //     'audioClearancePath' => 'calculator_clearance_09.mp3',
-        //     'audioFormulaPath' => 'calculator_formula_10.mp3',
-        // ];
+        $audioFiles = [
+            'audioIntroPath' => 'calculator_intro_01.mp3',
+            'audioConfigPath' => 'calculator_config_04.mp3',
+            'audioPartsPath' => 'calculator_parts_02.mp3',
+            'audioFunctionsPath' => 'calculator_functions_03.mp3',
+            'audioUsePath' => 'calculator_use_05.mp3',
+            'audioUnitPath' => 'calculator_unit_06.mp3',
+            'audioFractionPath' => 'calculator_fraction_07.mp3',
+            'audioHierarchyPath' => 'calculator_hierarchy_08.mp3',
+            'audioClearancePath' => 'calculator_clearance_09.mp3',
+            'audioFormulaPath' => 'calculator_formula_10.mp3',
+        ];
         
-        // $audioPaths = [];
-        // foreach ($audioFiles as $key => $file) {
-        //     $audioPaths[$key] = $languageController->getAudioBasePath() . $file;
-        // }
+        $audioPaths = [];
+        foreach ($audioFiles as $key => $file) {
+            $audioPaths[$key] = $languageController->getAudioBasePath() . $file;
+        }
         
-        // return view('Calculator.menuCalculator', $audioPaths);
-        return view('Calculator.menuCalculator');
-
+        return view('Calculator.menuCalculator', $audioPaths);
 
        // return view('Calculator.menuCalculator', compact('audioIntroPath', 'audioConfigPath', 'audioPartsPath', 'audioFunctionsPath', 'audioUsePath', 'audioUnitPath',  'audioFractionPath', 'audioHierarchyPath', 'audioClearancePath', 'audioFormulaPath'));
     }

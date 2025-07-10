036333bfbf5788caf4720e1c7711b7d8a9c14181
LindaSosa <admonti@results-in-performance.com>
2025-07-10 10:23:22 -0600

    configuracion final vista de evaluaciones

--- resources/views/Evaluation/mainEvaluation.blade.php
+++ resources/views/Evaluation/mainEvaluation.blade.php
@@ -24,7 +24,7 @@
                         <div class="stat-label">Experto en pozo</div>
                     </div>
                     <div class="insignia-icon"> 
-                        <img src="/assets/images/evaluation/elements/expert.png" alt="racha">
+                        <img src="/assets/images/evaluation/elements/expertC.png" alt="racha">
                     </div>
                 </div>
                 <div class="stat-category">INSIGNIA</div>
@@ -59,82 +59,99 @@
 
     </div>
     <div class="main-content">
-            <div class="main-left">
-                <div class="main-title-card">
-                    <img src="/assets/images/evaluation/inge.png" alt="Ingeniero" class="engineer-image">
-                    <div class="title-content">
-                        <h1 class="main-title">Evaluaciones</h1>
-                        <h2 class="subtitle">de Control de Pozos</h2>
-                    </div>
-                </div>
-               
-                <div class="bottom-section">
-                     <div class="slide">
-                    <div class="slide-inner">
-                        <input class="slide-open" type="radio" id="slide-1" 
-                            name="slide" aria-hidden="true" hidden="" checked="checked">
-                        <div class="slide-item">
-                            <img src="/assets/images/evaluation/petroleros6.jpg">
-                        </div>
-                        <input class="slide-open" type="radio" id="slide-2" 
-                            name="slide" aria-hidden="true" hidden="">
-                        <div class="slide-item">
-                            <img src="/assets/images/evaluation/petroleros2.jpg">
-                        </div>
-                        <input class="slide-open" type="radio" id="slide-3" 
-                            name="slide" aria-hidden="true" hidden="">
-                        <div class="slide-item">
-                            <img src="/assets/images/evaluation/petroleros5.jpg">
-                        </div>
-                        <label for="slide-3" class="slide-control prev control-1">‹</label>
-                        <label for="slide-2" class="slide-control next control-1">›</label>
-                        <label for="slide-1" class="slide-control prev control-2">‹</label>
-                        <label for="slide-3" class="slide-control next control-2">›</label>
-                        <label for="slide-2" class="slide-control prev control-3">‹</label>
-                        <label for="slide-1" class="slide-control next control-3">›</label>
-                        <ol class="slide-indicador">
-                            <li>
-                                <label for="slide-1" class="slide-circulo">•</label>
-                            </li>
-                            <li>
-                                <label for="slide-2" class="slide-circulo">•</label>
-                            </li>
-                            <li>
-                                <label for="slide-3" class="slide-circulo">•</label>
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-
-                
-                    <div class="content-card">
-                        <div class="content-title">Contenido</div>
-                        <div class="content-number">5</div>
-                        <div class="content-description">
-                            evaluaciones de práctica
-                            <br><br>
-                            nuevas asignaciones durante tu curso de Control de Pozos
-                        </div>
-                        <img src="/assets/images/evaluation/inge2.png" alt="Trabajador" class="content-workers-image">
-                    </div>
+        <div class="main-left">
+            <div class="main-title-card">
+                <img src="/assets/images/evaluation/inge.png" alt="Ingeniero" class="engineer-image">
+                <div class="title-content">
+                    <div class="main-title">Evaluaciones</div>
+                    <h2 class="subtitle">de Control de Pozos</h2>
                 </div>
             </div>
             
-            <div class="welcome-card">
-                <div class="welcome-title">Bienvenido, Ingeniero.</div>
-                <div class="welcome-subtitle">Soy RIPER!</div>
-                <div class="welcome-subtitle">seré tu ayudante en este curso de Control de Pozos</div>
-                
-                <div class="mascot-container">
-                    <div class="mascot-character">
-                        <img src="/assets/images/principal/castorSaludando.png" alt="Ingeniero" >
+            <div class="bottom-section">
+                    <div class="slide">
+                <div class="slide-inner">
+                    <input class="slide-open" type="radio" id="slide-1" 
+                        name="slide" aria-hidden="true" hidden="" checked="checked">
+                    <div class="slide-item">
+                        <div class="slide-texts"> 
+                            <div class="slide-title">Progreso evolutivo</div> 
+                            <div class="slide-text">Desarrolla habilidades clave a través de exámenes prácticos diseñados por expertos.</div>
+                        </div>
+                        <img src="/assets/images/evaluation/petroleros6.jpg">
                     </div>
-                    <div class="mascot-base">
-                        <img src="/assets/images/principal/pasto.png" alt="Ingeniero" >
+                    <input class="slide-open" type="radio" id="slide-2" 
+                        name="slide" aria-hidden="true" hidden="">
+                    <div class="slide-item">
+                        <div class="slide-texts"> 
+                            <div class="slide-title">Simulación real de examen</div> 
+                            <div class="slide-text">Practica con exámenes que replican el formato y condiciones del examen final ante IWCF o IADC.</div>
+                        </div>
+                        <img src="/assets/images/evaluation/petroleros2.jpg">
                     </div>
+                    <input class="slide-open" type="radio" id="slide-3" 
+                        name="slide" aria-hidden="true" hidden="">
+                    <div class="slide-item">
+                        <div class="slide-texts"> 
+                            <div class="slide-title">Interfaz adaptable y amigable</div> 
+                            <div class="slide-text">Accede desde cualquier dispositivo y mantén tu progreso en cada sesión, estés donde estés.</div>
+                        </div>
+                        <img src="/assets/images/evaluation/petroleros5.jpg">
+                    </div>
+                    <label for="slide-3" class="slide-control prev control-1">‹</label>
+                    <label for="slide-2" class="slide-control next control-1">›</label>
+                    <label for="slide-1" class="slide-control prev control-2">‹</label>
+                    <label for="slide-3" class="slide-control next control-2">›</label>
+                    <label for="slide-2" class="slide-control prev control-3">‹</label>
+                    <label for="slide-1" class="slide-control next control-3">›</label>
+                    <ol class="slide-indicador">
+                        <li>
+                            <label for="slide-1" class="slide-circulo">•</label>
+                        </li>
+                        <li>
+                            <label for="slide-2" class="slide-circulo">•</label>
+                        </li>
+                        <li>
+                            <label for="slide-3" class="slide-circulo">•</label>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+
+            
+                <div class="content-card">
+                    <div class="content-title">Contenido</div>
+                    <div class="content-text-number">
+                        <div class="content-number">5</div>
+                        <div class="content-text"> evaluaciones  <br>de práctica</div>
+                    </div>
+                    <div class="content-plus">
+                        +
+                    </div>
+                    <div class="content-description">
+                        nuevas asignaciones durante <br>tu curso de <strong>Control de Pozos</strong>
+                    </div>
+                    <img src="/assets/images/evaluation/inge2.png" alt="Trabajador" class="content-workers-image">
                 </div>
             </div>
         </div>
+        
+        <div class="welcome-card">
+            <div class="welcome-title">Bienvenido, Ingeniero.</div>
+            <div class="welcome-subtitle">Soy RIPER!</div>
+            <div class="welcome-subtitle">seré tu ayudante en este curso de Control de Pozos</div>
+            
+            <div class="mascot-container">
+                <div class="mascot-character">
+                    <img src="/assets/images/principal/castorSaludando.png" alt="Ingeniero" >
+                </div>
+                <div class="mascot-base">
+                    <img src="/assets/images/principal/pasto.png" alt="Ingeniero" >
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
 
 <script src="/js/Evaluation/Evaluation.js?v=1.0"></script>
--- resources/views/Template/maestraUser.blade.php
+++ resources/views/Template/maestraUser.blade.php
@@ -22,7 +22,8 @@ use Illuminate\Support\Str;
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Niconne&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Poppins" rel='stylesheet'>
+    <!-- <link href="https://fonts.googleapis.com/css?family=Poppins" rel='stylesheet'> -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flipbook.js/0.0.1/flipbook.min.css">
     
